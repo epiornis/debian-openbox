0e646a1d0cb92855948b672e1dbb6d73dc1a2e0d
epiornis <134094546+epiornis@users.noreply.github.com>
2025-10-13 12:18:19 +0200

    Update install.sh

--- 00_add_repo/install.sh
+++ 00_add_repo/install.sh
@@ -9,7 +9,6 @@
 # Check root
 [ "$(id -u)" -ne 0 ] && { echo "Must run as root" 1>&2; exit 1; }
 
-
 (
 # Add contrib section
 deb_lines_contrib="$(egrep '^(deb|deb-src) (http://deb.debian.org/debian/|http://security.debian.org/debian-security)' /etc/apt/sources.list | grep -v contrib)"
